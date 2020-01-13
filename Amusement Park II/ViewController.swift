b3d021172b98f5ca60252ff081fc258680c0c428
Michael R.F <mrflowers22@uwalumni.com>
2020-01-13 08:15:17 -0800

    first commit

---
+++ Amusement Park II/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amusement Park II
+//
+//  Created by Michael Flowers on 1/13/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
