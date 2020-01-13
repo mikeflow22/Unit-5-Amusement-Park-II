fba846fd314a564afff073d803d58d45445a70cf
Michael R.F <mrflowers22@uwalumni.com>
2020-01-13 08:20:03 -0800

    organized the project navigator

---
+++ Amusement Park II/Model Types/EntrantType.swift
@@ -0,0 +1,16 @@
+//
+//  EntrantType.swift
+//  Amusement
+//
+//  Created by Michael Flowers on 9/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+enum EntrantType: String {
+    case guest = "Guest"
+    case hourlyEmployee = "HourlyEmployee"
+    case manager = "Manager"
+    case vendor = "Vendor"
+    case contractor = "Contractor"
+}
